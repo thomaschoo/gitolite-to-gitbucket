6d1a15ed01103834c1e0d151dd54bf815bef7f7b
Thomas Choo <c.thomas.r@gmail.com>
2015-08-09 17:29:49 -0700

    Add more file helpers

--- src/main/scala/com/thomaschoo/KeyReader.scala
+++ src/main/scala/com/thomaschoo/KeyReader.scala
@@ -9,4 +9,6 @@ import scala.io.Source
 class KeyReader {
   def listFiles(): List[File] = new File(GitoliteConfig.directory).listFiles.toList
   def retrieveContents(file: File): String = Source.fromFile(file).getLines().mkString
+  def getFilename(file: File): String = file.getName
+  def getFilenameWithoutExtension(file: File): String = getFilename(file).replaceAll("\\.[^.]*$", "")
 }
